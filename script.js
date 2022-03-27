72b84c43bf52d9abf7bb7effc4b57ddb63c7487f
Gabriel Gomes Rodrigues <gabrielbj99@gmail.com>
2022-03-27 10:56:29 -0300

    Requisitos 3 e 4

--- script.js
+++ script.js
@@ -1,10 +1,30 @@
 // Cria um valor aleatório de RGB e coloca no parágrafo como string (cor a ser adivinhada)
 function randomColor() {
-  const rgb = `(${Math.floor(Math.random() * 255)}, ` +
+  const colorGuess = `(${Math.floor(Math.random() * 255)}, ` +
    `${Math.floor(Math.random() * 255)}, ` +
    `${Math.floor(Math.random() * 255)})`
-  document.getElementById('rgb-color').innerText = rgb;
+  document.getElementById('rgb-color').innerText = colorGuess;
+  return colorGuess;
 }
 
-randomColor();
+function addColors() {
+  const rgb = randomColor();
+  const positionColor = Math.floor(Math.random() * 6)
 
+  for (let index = 0; index < 6; index += 1) {
+    const color = document.createElement('div');
+    color.className = 'ball';
+
+    if (index === positionColor) {
+      color.style.backgroundColor = `rgb${rgb}`
+    } else {
+      color.style.backgroundColor = `rgb(${Math.floor(Math.random() * 255)},
+      ${Math.floor(Math.random() * 255)},
+      ${Math.floor(Math.random() * 255)})`;
+    }
+    
+    document.getElementById('colors').appendChild(color)
+  }
+}
+
+addColors()
