9a0110b1749ce12d438f7c63a5e4dec40b26544b
Gabriel Gomes Rodrigues <gabrielbj99@gmail.com>
2022-03-27 11:38:43 -0300

    Requisitos 5 e 6

--- script.js
+++ script.js
@@ -2,11 +2,13 @@
 function randomColor() {
   const colorGuess = `(${Math.floor(Math.random() * 255)}, ` +
    `${Math.floor(Math.random() * 255)}, ` +
-   `${Math.floor(Math.random() * 255)})`
+   `${Math.floor(Math.random() * 255)})`;
   document.getElementById('rgb-color').innerText = colorGuess;
   return colorGuess;
 }
 
+// Cria 6 divs para a section com id colors e adiciona cores aleatórias a cada uma, senda uma das cores igual a do código rgb criado
+// pela função randomColor
 function addColors() {
   const rgb = randomColor();
   const positionColor = Math.floor(Math.random() * 6)
@@ -16,15 +18,40 @@ function addColors() {
     color.className = 'ball';
 
     if (index === positionColor) {
-      color.style.backgroundColor = `rgb${rgb}`
+      color.style.backgroundColor = `rgb${rgb}`;
+      color.id = 'correct';
     } else {
       color.style.backgroundColor = `rgb(${Math.floor(Math.random() * 255)},
       ${Math.floor(Math.random() * 255)},
       ${Math.floor(Math.random() * 255)})`;
     }
     
-    document.getElementById('colors').appendChild(color)
+    document.getElementById('color-option').appendChild(color);
+
+    [...document.getElementsByClassName('ball')].forEach(element => {
+    element.addEventListener('click', chosenColor);
+    });
   }
 }
 
-addColors()
+addColors();
+
+// Exibe se a cor escolhida pelo usuário está certa ou não
+function chosenColor() {
+  this.style.borderColor = 'red';
+  if (this.id === 'correct') {
+    document.getElementById('answer').innerText = 'Acertou!';
+  } else {
+    document.getElementById('answer').innerText = 'Errou! Tente novamente!';
+  }
+}
+
+// Reseta as cores ao clique do botão e aleatoriza tudo novamente
+function reset() {
+  randomColor();
+  document.getElementById('color-option').innerHTML = '';
+  addColors();
+  document.getElementById('answer').innerText = 'Escolha uma cor';
+}
+
+document.getElementById('reset-game').addEventListener('click', reset);
